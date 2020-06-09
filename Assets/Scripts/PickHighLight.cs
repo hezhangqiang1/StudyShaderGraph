edbd078627c0cf07f3cb5132a1f0abd79884e73f
hezhangqiang <1025440064@qq.com>
2020-06-09 23:07:39 +0400

    highlight

---
+++ Assets/Scripts/PickHighLight.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickHighLight : MonoBehaviour
+{
+    private MeshRenderer originalMesh;
+    [SerializeField]
+    private MeshRenderer highLightMesh;
+
+    private void Start()
+    {
+        originalMesh = GetComponent<MeshRenderer>();
+        EnableHighLightMesh(false);
+    }
+
+    private void EnableHighLightMesh(bool onOff)
+    {
+        if (highLightMesh != null)
+        {
+            highLightMesh.enabled = onOff;
+            originalMesh.enabled = !onOff;
+        }
+    }
+
+    private void OnMouseEnter()
+    {
+        EnableHighLightMesh(true);
+    }
+    private void OnMouseExit()
+    {
+        EnableHighLightMesh(false);
+    }
+
+}
